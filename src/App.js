2d9520d90ee64e3af4f704b4f705c6e19e6cf159
Frode <frode@fintlabs.no>
2019-08-06 23:54:00 +0200

    refactored layout to use grid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
+import logo from './images/l4d-logo.png' ;
 
 function App() {
     return (
-        <div className="root">
-            <div className="background-image"></div>
-            <div className="title">
-                <h1>Logs4Dogs</h1>
-                <p>Think - Plan - Do</p>
-            </div>
-            <div className="content">
+        <div className="App">
+
+            <div className="grid-container">
+                <div className="logo">
+                    <img src={logo}/>
+                </div>
+                <div className="title">
+                    <h1>Logs4Dogs</h1>
+                    <p>Think - Plan - Do</p>
+                </div>
                 <div className="about">
                     <p>
                         Logs4Dogs is a app that will help you be a better trainer by:
@@ -19,17 +23,29 @@ function App() {
                         <li>collect data form your training sessions</li>
                         <li>analyse your training data</li>
                     </ul>
+                    <p>In short, the app will help to monitoring your progress in training.</p>
                     <div className="demo-button-container">
-                    <div className="demo-button">
-                        <button className="button button-no-spacing">Want to watch a demo?</button>
-                    </div>
+                        <div className="demo-button">
+                            <button className="button button-no-spacing">Want to watch a demo?</button>
+                        </div>
                     </div>
                 </div>
-                <div className="buttons">
+                <div className="beta">
                     <button className="button button-inverted button-spacing">Join beta testing iOS</button>
                     <button className="button button-inverted button-spacing">Join beta testing Android</button>
                 </div>
+                <div className="sponsor">
+                    <div className="sponsor-logo-container">
+                        <img src="https://www.simonprins.com/wp-content/uploads/Logo-Simon-ACT-websitelogo.png" alt="ACT logo"/>
+                    </div>
+                </div>
+                <div className="copyright">
+                    <p className="code">
+                       Logs4Dogs - Copyright © 2019 Frode Sjovatsen.
+                    </p>
+                </div>
             </div>
+
         </div>
     );
 }
